e0c385661bad20c9f95031525ef6a986070baa8f
Trace <d.nguyen@live.dk>
2026-04-01 10:10:54 +0200

    added stuff to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { useRoute } from 'vue-router'
+import Header from '@/layouts/Header.vue'
+import Navigation from '@/layouts/Navigation.vue'
 
+const route = useRoute()
 </script>
 
 <template>
-  
+  <Header v-if="route.name !== 'login'" />
+  <RouterView />
+  <Navigation v-if="route.name !== 'login'" />
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
 </style>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,7 +1,9 @@
 <script setup>
+import ChatInputField from '@/components/chatComponents/ChatInputField.vue'
 </script>
 
 <template>
+  <ChatInputField @send="(msg) => console.log(msg)" />
   <div class="chat-view"></div>
 </template>
 
