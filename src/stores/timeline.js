4d6d74910bb951ec546b3c0151f5a1ec57b3f150
alch29 <christensen.alek@gmail.com>
2026-04-23 09:51:10 +0200

    refactoring of timeline store

--- src/stores/timeline.js
+++ src/stores/timeline.js
@@ -64,7 +64,6 @@ export const useTimelineStore = defineStore('timeline', () => {
     previewItems,
     formatDate, 
     getVariant,
-    fetchTimeline,
-    addEvent
+    fetchTimeline
   };
 });
